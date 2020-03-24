9ea9c6ad1dd52d8e5aa185236a5c686a906428b8
danielsworld <didacticdaniel@gmail.com>
2020-03-24 15:11:10 -0500

    Created main menu structure

---
+++ app/src/main/java/edu/apsu/csci/CalorieCounter/MenuActivity.java
@@ -0,0 +1,25 @@
+/*
+    Authors: Daniel Davis, Dalton Claxton, Peyton White
+    Date: 30 March 2020
+    Description: A simple calorie counting app using API data from the US Department of Agriculture
+ */
+
+package edu.apsu.csci.CalorieCounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    public void onExitButton(final View view) {
+        finish();
+    }
+}
