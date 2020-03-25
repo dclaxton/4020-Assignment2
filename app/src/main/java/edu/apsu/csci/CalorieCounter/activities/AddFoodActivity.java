69fb0d023f380a776963da0d74f40f6d72937831
danielsworld <didacticdaniel@gmail.com>
2020-03-24 19:29:11 -0500

    Added date picking functionality to the AddFoodActivity

--- app/src/main/java/edu/apsu/csci/CalorieCounter/activities/AddFoodActivity.java
+++ app/src/main/java/edu/apsu/csci/CalorieCounter/activities/AddFoodActivity.java
@@ -6,7 +6,57 @@
 
 package edu.apsu.csci.CalorieCounter.activities;
 
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import edu.apsu.csci.CalorieCounter.R;
+import edu.apsu.csci.CalorieCounter.listeners.GoToActivity;
+
 public class AddFoodActivity extends AppCompatActivity {
+    private final Calendar mCalendar = Calendar.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_food);
+
+        findViewById(R.id.to_menu_button).setOnClickListener(new GoToActivity(this, MenuActivity.class));
+        findViewById(R.id.set_date_edit_text).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(AddFoodActivity.this, date, mCalendar
+                        .get(Calendar.YEAR), mCalendar.get(Calendar.MONTH),
+                        mCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            mCalendar.set(Calendar.YEAR, year);
+            mCalendar.set(Calendar.MONTH, monthOfYear);
+            mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateDate();
+        }
+
+    };
+
+    private void updateDate() {
+        ((EditText) findViewById(R.id.set_date_edit_text)).setText(
+                new SimpleDateFormat("MM/dd/yyyy", Locale.US).format(mCalendar.getTime()));
+        ((TextView) findViewById(R.id.weekday_text_view)).setText(
+                new SimpleDateFormat("EEEE", Locale.US).format(mCalendar.getTime()));
+    }
 }
--- app/src/main/java/edu/apsu/csci/CalorieCounter/db/Database.java
+++ app/src/main/java/edu/apsu/csci/CalorieCounter/db/Database.java
@@ -1,4 +1,4 @@
 package edu.apsu.csci.CalorieCounter.db;
 
-public class DataBase {
+public class Database {
 }
