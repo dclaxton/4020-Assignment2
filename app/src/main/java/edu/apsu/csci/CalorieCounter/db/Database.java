437f9cac10fc3a8e3c6c4c60b6aba89a0ff6aef3
pwhite15 <pwhite15@my.apsu.edu>
2020-03-24 21:31:29 -0500

    db comments nothing really

--- app/src/main/java/edu/apsu/csci/CalorieCounter/db/Database.java
+++ app/src/main/java/edu/apsu/csci/CalorieCounter/db/Database.java
@@ -1,4 +1,20 @@
 package edu.apsu.csci.CalorieCounter.db;
 
 public class Database {
+
+
+    //database name
+
+    //database tables
+
+    //Columns
+
+
+    //create samples to start off
+
+    //functions to help
+
+
+
+
 }
