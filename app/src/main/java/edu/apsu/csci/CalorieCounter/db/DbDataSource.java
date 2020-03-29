0df8cb0d98c16515ea63b38710ceac6127669609
dalton <dclaxton1@my.apsu.edu>
2020-03-29 00:09:34 -0500

    cleaned up whitespace

--- app/src/main/java/edu/apsu/csci/CalorieCounter/db/DbDataSource.java
+++ app/src/main/java/edu/apsu/csci/CalorieCounter/db/DbDataSource.java
@@ -18,20 +18,17 @@ import java.util.Locale;
 import edu.apsu.csci.CalorieCounter.classes.Food;
 
 public class DbDataSource {
-
     private SQLiteDatabase database;
     private MySqlLiteHelper databaseHelper;
 
     public DbDataSource(Context context)
     {
         databaseHelper = new MySqlLiteHelper(context);
-
     }
 
     public void open()
     {
         database = databaseHelper.getWritableDatabase();
-
     }
 
     public void close()
@@ -40,72 +37,51 @@ public class DbDataSource {
     }
 
     public List<Food> getAllFood() {
-
         List<Food> foods = new ArrayList<>();
-
         String columns[] = MySqlLiteHelper.DetailsColumns.names();
-
         Cursor cursor = database.query(MySqlLiteHelper.FOOD_DETAILS_TABLE,columns,null,null,null,null, null);
         cursor.moveToFirst();
-
         while(!cursor.isAfterLast()) {
             Food food = cursorToFood(cursor);
             foods.add(food);
             cursor.moveToNext();
             Log.i("SQL CHECK","check: " + food.getCalories());
         }
-
         cursor.close();
-
-
         return foods;
-
     }
 
     public Food createFood(String foodName,int id,String dateCreated,double calories)
     {
         ContentValues contentValues = new ContentValues();
-
         //any data we want to put into the DB we add to content values
         //put in food name
         // put id into it
         contentValues.put(MySqlLiteHelper.DetailsColumns.food_id.toString(),id);
         contentValues.put(MySqlLiteHelper.DetailsColumns.food_name.toString(),foodName);
-
         // put cal into it
         contentValues.put(MySqlLiteHelper.DetailsColumns.food_calories.toString(),calories);
         ///put in data created
         contentValues.put(MySqlLiteHelper.DetailsColumns.data_created.toString(),dateCreated);
-
-
         //fill out all fields then insert it
-
         //insert into food table food name, food id, datecreated, calories
          open();
          database.insert(MySqlLiteHelper.FOOD_DETAILS_TABLE,null,contentValues);
-
-
         //might need to seperate this into a different function called getfoodfromdb()
         String[] columnNames = MySqlLiteHelper.DetailsColumns.names();
-
         //get back the food we just created
         Cursor cursor = database.query(MySqlLiteHelper.FOOD_DETAILS_TABLE, columnNames,MySqlLiteHelper.DetailsColumns.food_id
                          + " = " + id,null,null,null,null);
-
         cursor.moveToFirst();
-
         Food food = cursorToFood(cursor);
         cursor.close();
         close();
-
         return food;
-
     }
 
     public void addFoodToDb(String foodString,int id,String dateCreated,double calories)
     {
         ContentValues contentValues = new ContentValues();
-
         //any data we want to put into the DB we add to content values
         //put in food name
         contentValues.put(MySqlLiteHelper.DetailsColumns.food_name.toString(),foodString);
@@ -116,32 +92,22 @@ public class DbDataSource {
         //put cal into it
         contentValues.put(MySqlLiteHelper.DetailsColumns.food_calories.toString(),calories);
         //fill out all fields then insert it
-
         //insert into food table food name, food id, datecreated, calories
-
         database.insert(MySqlLiteHelper.FOOD_DETAILS_TABLE,null,contentValues);
-
-
     }
 
     private Food cursorToFood(Cursor cursor)
     {
         Food food = new Food();
-
         int foodId = cursor.getInt(MySqlLiteHelper.DetailsColumns.food_id.ordinal());
         food.setId(foodId);
-
         String foodName = cursor.getString(MySqlLiteHelper.DetailsColumns.food_name.ordinal());
         food.setName(foodName);
-
         String datestr = cursor.getString(MySqlLiteHelper.DetailsColumns.data_created.ordinal());
         food.setDateCreated(datestr);
-
         double calories = cursor.getDouble(MySqlLiteHelper.DetailsColumns.food_calories.ordinal());
         food.setCalories(calories);
-
         DateFormat dateFormat = new SimpleDateFormat("EEE MMM DD kk:mm:ss z yyyy", Locale.ENGLISH);
-
         try {
             Date date = dateFormat.parse(datestr);
             // can clear the comment when date is made for food class
@@ -149,10 +115,6 @@ public class DbDataSource {
         } catch (ParseException e) {
             e.printStackTrace();
         }
-
-
         return food;
-
     }
-
 }
